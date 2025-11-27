e2181e01fb6cf0b8ef5395bc0f744a30587da834
Anders Bjerner <abj@limbo.works>
2025-11-27 14:27:31 +0100

    Added misc "TryGet" style extension methods for "HttpRequest"

--- src/Skybrud.Essentials.AspNetCore/RequestExtensions.cs
+++ src/Skybrud.Essentials.AspNetCore/RequestExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
 
 namespace Skybrud.Essentials.AspNetCore;
 
@@ -85,6 +87,72 @@ public static class RequestExtensions {
     /// <returns>The value of the header, or <c>null</c> if not present.</returns>
     public static string? GetForwardedFor(this HttpRequest? request) {
         return request?.Headers["X-Forwarded-For"].FirstOrDefault();
+    }
+
+    /// <summary>
+    /// Attempts to get the value of the <c>X-Forwarded-For</c> header.
+    /// </summary>
+    /// <param name="request">The request.</param>
+    /// <param name="result">When this method returns, holds the value of the <c>X-Forwarded-For</c> header if successful; otherwise, <see langword="null"/>.</param>
+    /// <returns><see langword="true"/> if successful; otherwise, <see langword="false"/>.</returns>
+    public static bool TryGetForwardedFor(this HttpRequest? request, [NotNullWhen(true)] out string? result) {
+        result = request?.Headers.TryGetValue("X-Forwarded-For", out StringValues values) is true ? values.FirstOrDefault() : null;
+        return !string.IsNullOrEmpty(result);
+    }
+
+    /// <summary>
+    /// Attempts to get the value of the <c>X-Forwarded-Host</c> header.
+    /// </summary>
+    /// <param name="request">The request.</param>
+    /// <param name="result">When this method returns, holds the value of the <c>X-Forwarded-Host</c> header if successful; otherwise, <see langword="null"/>.</param>
+    /// <returns><see langword="true"/> if successful; otherwise, <see langword="false"/>.</returns>
+    public static bool TryGetForwardedHost(this HttpRequest? request, [NotNullWhen(true)] out string? result) {
+        result = request?.Headers.TryGetValue("X-Forwarded-Host", out StringValues values) is true ? values.FirstOrDefault() : null;
+        return !string.IsNullOrEmpty(result);
+    }
+
+    /// <summary>
+    /// Attempts to get the value of the <c>X-Forwarded-Proto</c> header.
+    /// </summary>
+    /// <param name="request">The request.</param>
+    /// <param name="result">When this method returns, holds the value of the <c>X-Forwarded-Proto</c> header if successful; otherwise, <see langword="null"/>.</param>
+    /// <returns><see langword="true"/> if successful; otherwise, <see langword="false"/>.</returns>
+    public static bool TryGetForwardedProto(this HttpRequest? request, [NotNullWhen(true)] out string? result) {
+        result = request?.Headers.TryGetValue("X-Forwarded-Proto", out StringValues values) is true ? values.FirstOrDefault() : null;
+        return !string.IsNullOrEmpty(result);
+    }
+
+    /// <summary>
+    /// Attempts to get the value of the <c>Referer</c> header.
+    /// </summary>
+    /// <param name="request">The request.</param>
+    /// <param name="result">When this method returns, holds the value of the <c>Referer</c> header if successful; otherwise, <see langword="null"/>.</param>
+    /// <returns><see langword="true"/> if successful; otherwise, <see langword="false"/>.</returns>
+    public static bool TryGetReferrer(this HttpRequest? request, [NotNullWhen(true)] out string? result) {
+        result = request?.Headers.TryGetValue("Referer", out StringValues values) is true ? values.FirstOrDefault() : null;
+        return !string.IsNullOrEmpty(result);
+    }
+
+    /// <summary>
+    /// Attempts to get the remote address of the specified <paramref name="request"/>.
+    /// </summary>
+    /// <param name="request">The request.</param>
+    /// <param name="result">When this method returns, holds the remote address if successful; otherwise, <see langword="null"/>.</param>
+    /// <returns><see langword="true"/> if successful; otherwise, <see langword="false"/>.</returns>
+    public static bool TryGetRemoteAddress(this HttpRequest? request, [NotNullWhen(true)] out string? result) {
+        result = request?.HttpContext.Connection.RemoteIpAddress?.ToString();
+        return !string.IsNullOrEmpty(result);
+    }
+
+    /// <summary>
+    /// Attempts to get the value of the <c>User-Agent</c> header.
+    /// </summary>
+    /// <param name="request">The request.</param>
+    /// <param name="result">When this method returns, holds the value of the <c>User-Agent</c> header if successful; otherwise, <see langword="null"/>.</param>
+    /// <returns><see langword="true"/> if successful; otherwise, <see langword="false"/>.</returns>
+    public static bool TryGetUserAgent(this HttpRequest? request, [NotNullWhen(true)] out string? result) {
+        result = request?.Headers.TryGetValue("User-Agent", out StringValues values) is true ? values.FirstOrDefault() : null;
+        return !string.IsNullOrEmpty(result);
     }
 
 }
